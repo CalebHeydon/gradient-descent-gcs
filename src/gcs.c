2d8817e9696498d425e3bb1893ad9689725ddae6
Caleb Heydon <caleb.heydon@gmail.com>
2022-12-26 14:39:40 -0500

    Fixed memory issue

--- src/gcs.c
+++ src/gcs.c
@@ -7,6 +7,7 @@ All rights reserved.
 
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 int gcs_destroy_node(gcs_node *node)
 {
@@ -27,8 +28,8 @@ int gcs_create_point(gcs_node **node, double x, double y)
         return -1;
     memset(*node, 0, sizeof(**node) + sizeof(double) * 2 + sizeof(bool) * 2);
 
-    (*node)->values = (void *)(*node + sizeof(**node));
-    (*node)->fixed = (void *)(*node + sizeof(**node) + sizeof(double) * 2);
+    (*node)->values = (uint8_t *)*node + sizeof(**node);
+    (*node)->fixed = (uint8_t *)*node + sizeof(**node) + sizeof(double) * 2;
 
     (*node)->values[0] = x;
     (*node)->values[1] = y;
